7dd1e10509a890c3669b07694b09b3c9f8955cde
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2018-12-12 20:39:07 -0500

    Add comments to model.js

--- src/model.js
+++ src/model.js
@@ -1,23 +1,35 @@
 // model.js
 // TensorFlow.js model creation code using high-level layers API
 
+// Learning rate for optimizer
 const learningRate = 0.25;
+// Optimizer to train model with
 const optimizer = tf.train.sgd(learningRate);
 
+// Input layer of model
 const input = tf.input({
       shape: [4]
 });
+// Hidden layers of neural network (between input and output layers)
 const layers = [
+      // Hidden layer 1
       tf.layers.dense({
             units: 4,
             activation: "tanh"
       }),
+      // Hidden layer 2
       tf.layers.dense({
             units: (2 * dimensions) + 2 + 4
       })
 ];
-const output = layers[1].apply(layers[0].apply(input));
+// Define flow of data between layers of network
+const output = layers[1].apply(
+      layers[0].apply(
+            input
+      )
+);
 
+// Create model with TensorFlow.js
 const model = tf.model({
       inputs: input,
       outputs: output
