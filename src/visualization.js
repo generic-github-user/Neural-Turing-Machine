bad6b3e377ed6d8d5d99aef41877c4e170a5258d
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2018-12-14 16:12:20 -0500

    Add comments to visualization code

--- src/visualization.js
+++ src/visualization.js
@@ -1,5 +1,7 @@
+// Set up canvas
 var canvas = document.querySelector("#canvas");
 var ctx = canvas.getContext("2d");
+// Set canvas dimensions
 canvas.width = window.innerWidth;
 canvas.width = window.innerHeight;
 
@@ -10,15 +12,24 @@ const map = function(num, in_min, in_max, out_min, out_max) {
 }
 
 const display = function() {
+      // Get values from NTM memory
       var memory = ntm.memory.dataSync();
+      // Find minimum and maximum values of memory for range mapping
       var min = Math.min(...memory);
       var max = Math.max(...memory);
+      // Find width of one block
       var width = canvas.width / memory.length;
+      // Loop through all memory cells
       for (var i = 0; i < memory.length; i++) {
+            // Map value of memory cell to range of values
             var hue = map(memory[i], min, max, 250, 300);
+            // Set color of cell based on value
             ctx.fillStyle = "hsla(" + hue + ", 100%, 50%, 1)";
+            // Fill in block to represent memory cell
             ctx.fillRect(
+                  // x
                   width * i,
+                  // y
                   0,
                   width,
                   canvas.height
